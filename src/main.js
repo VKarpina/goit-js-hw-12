3e5c0a83a3e49dadcf9b274581acb112d4c4f090
VKarpina <v.karpina01@gmail.com>
2024-11-17 17:44:46 +0100

    add some fixes

--- src/main.js
+++ src/main.js
@@ -10,6 +10,20 @@ const form = document.querySelector('.form-container');
 const loader = document.querySelector('.loader');
 const loadBtn = document.querySelector('.load-btn');
 
+const iziToastStyles = {
+  titleColor: '#fff',
+  titleSize: '16px',
+  titleLineHeight: '1.5',
+  messageColor: '#fff',
+  messageSize: '16px',
+  messageLineHeight: '1.5',
+  backgroundColor: '#ef4040',
+  icon: 'ico-warning',
+  iconColor: '#fff',
+  iconUrl: '../img/bi_x-octagon.svg',
+  close: false,
+};
+
 form.addEventListener('submit', handleSearch);
 loadBtn.addEventListener('click', onLoadBtn);
 
@@ -23,17 +37,35 @@ async function handleSearch(event) {
   event.preventDefault();
 
   inputValue = event.currentTarget.elements.query.value.trim();
+
   if (inputValue === '') {
     return;
   }
+  gallery.innerHTML = '';
   showElement(loader);
-
+  page = 1;
   try {
     const data = await pixabayAPI(inputValue, page, perPage);
-    if (data.hits.length === 0) {
-      iziToast.info({ message: 'Photos not found, try another query' });
+
+    if (inputValue.includes(' ')) {
+      iziToast.info({
+        message: 'Query should not contain spaces.',
+        ...iziToastStyles,
+      });
+      hideElement(loadBtn);
       return;
     }
+
+    if (data.hits.length === 0) {
+      iziToast.info({
+        message: 'Photos not found, try another query',
+        ...iziToastStyles,
+      });
+      hideElement(gallery);
+      hideElement(loadBtn);
+      return;
+    }
+
     form.reset();
     gallery.innerHTML = renderFunction(data.hits);
 
@@ -47,7 +79,7 @@ async function handleSearch(event) {
     }
     lightbox.refresh();
   } catch (error) {
-    iziToast.error({ message: 'Something went wrong' });
+    iziToast.error({ message: 'Something went wrong', ...iziToastStyles });
   } finally {
     hideElement(loader);
   }
